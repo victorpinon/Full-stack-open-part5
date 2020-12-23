82cc5a11361568a2740afc960a81dc3aff72cb60
Víctor Piñón <victorpinonrattia@hotmail.com>
2020-12-23 20:29:44 +0100

    ex 5.7

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,36 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, {useState} from 'react'
+
+const Blog = ({ blog }) => {
+  const [details, setDetails] = useState(false)
+
+  const toggleDetails = () => {
+    setDetails(!details)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const getDetails = () => (
+    <div>
+      <p>{blog.url}</p>
+      <p>{blog.likes}</p>
+      <p>{blog.user.name}</p>
+    </div>
+  )
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button onClick={toggleDetails}>{details ? 'hide' : 'view'}</button>
+      </div>
+      {details && getDetails()}
+    </div>
+  )
+}
 
 export default Blog
