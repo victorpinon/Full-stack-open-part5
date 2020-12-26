39f65d98a4e7b32c9ebf19d4b0d5841758e1a8c4
Víctor Piñón <victorpinonrattia@hotmail.com>
2020-12-26 13:21:06 +0100

    ex 5.17

--- bloglist-backend/.eslintrc.js
+++ bloglist-backend/.eslintrc.js
@@ -2,13 +2,17 @@ module.exports = {
   'env': {
     'es2021': true,
     'node': true,
-    'jest': true
+    'jest': true,
+    'cypress/globals': true
   },
   'extends': 'eslint:recommended',
   'parserOptions': {
     'ecmaVersion': 12,
     'sourceType': 'module'
   },
+  'plugins': [
+    'react', 'jest', 'cypress'
+  ],
   'rules': {
     'indent': [
       'error',
